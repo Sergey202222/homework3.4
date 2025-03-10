31ddaa9e1a98ae5e2f8e5fce0105c46ed8641d94
Sergey <57405954+Sergey202222@users.noreply.github.com>
2025-03-10 20:05:16 +0300

    Add files via upload

---
+++ homework4.1/homework4.1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+class Figure
+{
+protected:
+    int sides_count;
+    std::string name_figure;
+public:
+    Figure()
+    {
+        sides_count = 0;
+        name_figure = "Фигура";
+    }
+    int get_sides_count()
+    {
+        return sides_count;
+    }
+    std::string get_name_figure()
+    {
+        return name_figure;
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+    Triangle()
+    {
+        sides_count = 3;
+        name_figure = "Треугольник";
+    }
+};
+
+class Quadrangle : public Figure
+{
+public:
+    Quadrangle()
+    {
+        sides_count = 4;
+        name_figure = "Четырёхугольник";
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Figure figure;
+    Triangle triangle;
+    Quadrangle quadrangle;
+
+    std::cout << "Количество сторон:\n";
+    std::cout << figure.get_name_figure() << ": " << figure.get_sides_count() << '\n';
+    std::cout << triangle.get_name_figure() << ": " << triangle.get_sides_count() << '\n';
+    std::cout << quadrangle.get_name_figure() << ": " << quadrangle.get_sides_count();
+    return EXIT_SUCCESS;
+}
